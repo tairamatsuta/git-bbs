8038ce4b3e2d1948817e700cd8faa521ebce0069
tairamatsuta <taira.matsuta@rakus.co.jp>
2020-07-09 14:12:36 +0900

    ドメインの実装

---
+++ src/main/java/com/example/domain/Comment.java
@@ -0,0 +1,57 @@
+package com.example.domain;
+
+/**
+ * コメント情報を表すドメイン.
+ * 
+ * @author taira.matsuta
+ *
+ */
+public class Comment {
+
+	/** コメントID */
+	private Integer id;
+	/** コメント投稿者名 */
+	private String name;
+	/** コメント内容 */
+	private String content;
+	/** 記事ID */
+	private Integer articleId;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", name=" + name + ", content=" + content + ", articleId=" + articleId + "]";
+	}
+	
+}
