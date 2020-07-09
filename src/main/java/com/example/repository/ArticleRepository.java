1f2cf640dba61e6617e84f058ea3df29f258b6f3
TakahashiKazuteru <kazuteru.takahashi@rakus.co.jp>
2020-07-09 14:41:34 +0900

    repositoryの修正

--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -1,9 +1,14 @@
 package com.example.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -11,60 +16,99 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Article;
+import com.example.domain.Comment;
 
 /**
- * articlesテーブルを操作するリポジトリ.
+ * 投稿記事を操作するリポジトリです.
  * 
- * @author taira.matsuta
+ * @author kazuteru.takahashi
  *
  */
 @Repository
 public class ArticleRepository {
-
-	/**
-	 * テーブル名
-	 */
-	private static final String TABLE_NAME = "articles";
-	
+	private static final String ARTICLES_TABLE_NAME = "articles";
+	private static final String COMMENTS_TABLE_NAME = "comments";
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = new BeanPropertyRowMapper<>(Article.class);
 	@Autowired
 	private NamedParameterJdbcTemplate template;
-	
+
 	/**
-	 * Articleオブジェクトを生成するローマッパー.
-	 */
-	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = new BeanPropertyRowMapper<>(Article.class);
-	
-	/**
-	 * 記事情報を全件検索する.
+	 * 投稿記事をすべて検索するメソッ
 	 * 
-	 * @return　記事情報
+	 * @return すべての投稿記事(0件ならばnull)
 	 */
 	public List<Article> findAll() {
-		String sql = "SELECT id, name, content FROM " + TABLE_NAME + " ORDER BY id DESC;";
-		//String sql = "SELECT a.id, a.name, a.content, c.id, c.name, c.content, c.article_id FROM articles a JOIN comments c ON a.id = c.article_id;";
+		String sql = "SELECT id,name,content FROM " + ARTICLES_TABLE_NAME + " ORDER BY id DESC;";
 		return template.query(sql, ARTICLE_ROW_MAPPER);
 	}
-	
+
 	/**
-	 * 記事情報を挿入する.
+	 * 記事情報とコメント情報を連結した結果をすべて検索するメソッド.
 	 * 
-	 * @param article
+	 * @return 記事情報とコメント情報の検索結果(コメントがない場合はコメント情報はnull)
+	 */
+	public List<Article> findAllWithComment() {
+		String sql = "SELECT a.id article_id,a.name article_name,a.content article_content,"
+				+ "b.id comment_id,b.name comment_name,b.content comment_content,b.article_id comment_article_id "
+				+ "FROM " + ARTICLES_TABLE_NAME + " a FULL JOIN " + COMMENTS_TABLE_NAME + " b on a.id = b.article_id "
+				+ "ORDER BY a.id DESC,b.id DESC;";
+		return template.query(sql, new ResultSetExtractor<List<Article>>() {
+			@Override
+			public List<Article> extractData(ResultSet rs) throws SQLException, DataAccessException {
+				List<Article> articleList = new ArrayList<>();
+				List<Comment> commentList = null;
+				Article article = null;
+
+				int beforeArticleId = -1;
+				while (rs.next()) {
+					if (rs.getInt("article_id") != beforeArticleId) {
+						article = new Article();
+						article.setId(rs.getInt("article_id"));
+						article.setName(rs.getString("article_name"));
+						article.setContent(rs.getString("article_content"));
+						//先に空のcommentListを生成しarticleにセットする
+						commentList = new ArrayList<>();
+						article.setCommentList(commentList);
+						
+						articleList.add(article);
+					}
+
+					if (article.getId() == rs.getInt("comment_article_id")) {
+						Comment comment = new Comment();
+						comment.setId(rs.getInt("comment_id"));
+						comment.setName(rs.getString("comment_name"));
+						comment.setContent(rs.getString("comment_content"));
+						comment.setArticleId(rs.getInt("comment_article_id"));
+						//articleListのarticleのcommentListを参照して挿入している
+						commentList.add(comment);
+					}
+					beforeArticleId = rs.getInt("article_id");
+				}
+				return articleList;
+			}
+		});
+	}
+
+	/**
+	 * 投稿記事をDBへ挿入するメソッド.
+	 * 
+	 * @param article 投稿記事
 	 */
 	public void insert(Article article) {
-		String insertSql = "INSERT INTO " + TABLE_NAME + "(name, content) VALUES(:name, :content);";
-		SqlParameterSource param = new MapSqlParameterSource().addValue("name", article.getName()).addValue("content", article.getContent());
-		template.update(insertSql, param);
+		String sql = "INSERT INTO " + ARTICLES_TABLE_NAME + "(name,content) values(:name,:content);";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", article.getName()).addValue("content",
+				article.getContent());
+		template.update(sql, param);
 	}
-	
+
 	/**
-	 * 記事情報を削除する.
+	 * 指定されたIDの投稿を削除するメソッド.
 	 * 
-	 * @param id 記事ID
+	 * @param id ID
 	 */
 	public void deleteById(int id) {
-		String deleteSql = "DELETE FROM " + TABLE_NAME + " WHERE id = :id;";
+		String sql = "DELETE FROM " + ARTICLES_TABLE_NAME + " WHERE id = :id;";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
-		template.update(deleteSql, param);
+		template.update(sql, param);
 	}
-	
 }
--- src/main/java/com/example/repository/CommentRepository.java
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -13,59 +14,51 @@ import org.springframework.stereotype.Repository;
 import com.example.domain.Comment;
 
 /**
- * commentsテーブルを操作するリポジトリ.
+ * コメント情報を操作するリポジトリです.
  * 
- * @author taira.matsuta
+ * @author kazuteru.takahashi
  *
  */
 @Repository
 public class CommentRepository {
-
-	/**
-	 * テーブル名
-	 */
 	private static final String TABLE_NAME = "comments";
-
+	private static final RowMapper<Comment> COMMENT_ROW_MAPPER = new BeanPropertyRowMapper<>(Comment.class);
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 	
 	/**
-	 * Commentオブジェクトを生成するローマッパー.
-	 */
-	private static final RowMapper<Comment> COMMENT_ROW_MAPPER = new BeanPropertyRowMapper<>(Comment.class);
-	
-	/**
-	 * コメント情報を検索する.(artile_idで検索)
+	 * 投稿記事IDから対応するコメントを検索するメソッド.
 	 * 
-	 * @param articleId　記事ID
-	 * @return　コメント情報
+	 * @param articleId 投稿記事ID
+	 * @return 投稿記事IDに対応したコメント(0件ならばnull)
 	 */
-	public List<Comment>findByArticleId(int articleId) {
-		String sql = "SELECT id, name, content, article_id FROM " + TABLE_NAME + " WHERE article_id = :articleId;";
-		SqlParameterSource param = new MapSqlParameterSource("articleId", articleId);
+	public List<Comment> findByArticleId(int articleId){
+		String sql = "SELECT id,name,content,article_id FROM "+TABLE_NAME+" "
+				+ "WHERE article_id = :articleId ORDER BY id DESC;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("articleId", articleId);
 		return template.query(sql, param, COMMENT_ROW_MAPPER);
 	}
 	
 	/**
-	 * コメント情報を挿入する.
+	 *投稿されたコメントを挿入するメソッド. 
 	 * 
-	 * @param comment コメント情報
+	 * @param comment 新規コメント
 	 */
 	public void insert(Comment comment) {
-		String insertSql = "INSERT INTO " + TABLE_NAME + "(name, content, article_id) VALUES(:name, :content, :articleId);";
-		SqlParameterSource param = new MapSqlParameterSource().addValue("name", comment.getName()).addValue("content", comment.getContent()).addValue("articleId", comment.getArticleId());
-		template.update(insertSql, param);
+		SqlParameterSource param = new BeanPropertySqlParameterSource(comment);
+		String sql= "INSERT INTO "+TABLE_NAME+"(name,content,article_id) VALUES(:name,:content,:articleId);";
+		template.update(sql, param);
 	}
 	
 	/**
-	 * コメント情報を削除する.
+	 * 指定された投稿記事IDのコメントをすべて削除するメソッド.
 	 * 
-	 * @param articleId　記事ID
+	 * @param articleId 投稿記事ID
 	 */
 	public void deleteByArticleId(int articleId) {
-		String deleteSql = "DELETE FROM " + TABLE_NAME + " WHERE article_id = :articleId;";
+		String sql = "DELETE FROM "+TABLE_NAME+" WHERE article_id = :articleId;";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("articleId", articleId);
-		template.update(deleteSql, param);
+		template.update(sql, param);
 	}
 	
 }
